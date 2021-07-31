eed566e113326fa47b7008f51eb753532f5be90a
Simon Andersen <simon.thye@gmail.com>
2021-07-31 23:25:17 +0200

    Added reading/writing of file to/from memory

--- src/main/scala/Slime.scala
+++ src/main/scala/Slime.scala
@@ -1,5 +1,11 @@
+package Slimer
+
 import chisel3._
 import chisel3.util._
+import chisel3.experimental.ChiselEnum
+
+
+import Memory._
 
 import chisel3.stage.ChiselStage
 
@@ -7,9 +13,58 @@ class Slime extends Module {
   val io = IO(new Bundle {
     val start = Input(Bool())
     val done = Output(Bool())
+
+
+    val dataOut = Output(UInt(32.W))
+    val dataIn = Input(UInt(32.W))
+    val readAll = Input(Bool())
+    val writeAll = Input(Bool())
+
   })
 
-  io.done := RegNext (io.start)
+  val memSize = 1024
+
+
+  object State extends ChiselEnum {
+    val sIdle, sMemFlush, sDone = Value
+  }
+
+  val state = RegInit(State.sIdle)
+  val memRW = Reg(Bool()) // 0 means R, 1 means W
+  val memCounter = Counter(memSize)
+
+  val mem = Mem(memSize,UInt(32.W))
+
+  io.done := 0.B
+
+
+  io.dataOut := RegNext(mem(memCounter.value))
+  switch(state){
+    is (State.sIdle){
+      when(io.readAll){
+        state := State.sMemFlush
+        memRW := 0.B
+      }
+      when(io.writeAll){
+        state := State.sMemFlush
+        memRW := 1.B
+      }
+    }
+    is (State.sMemFlush){
+      when(memCounter.inc()){
+        state := State.sDone
+      }
+      when(memRW){ //Write. Read is always done by counter
+        mem(memCounter.value) := io.dataIn
+      }
+    }
+    is (State.sDone){
+      state := State.sIdle
+      io.done := 1.B
+    }
+  }
+
+  
 }
 
 // generate Verilog
